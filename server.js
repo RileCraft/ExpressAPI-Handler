7d417c05d3633a362c25445d378c6583655e52b8
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-09-30 14:23:18 +0530

    update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const { expressHandler } = require(__dirname + "/Home/Classes/ExpressHandler");
 global.rootPATH = __dirname
 global.apiPATH = __dirname + "/Home/Endpoints"
 
+const { expressHandler } = require(__dirname + "/Home/Classes/ExpressHandler");
 expressHandler.loadAPIs()     // LOADAPIS HANDLER
